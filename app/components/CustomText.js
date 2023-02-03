ff2c309ac8d118b69031bc8e063619e1631b06b2
Ahmed Kasu <ankassu@gmail.com>
2023-02-03 12:03:32 +0200

    Added default colors to the color palette

--- app/components/CustomText.js
+++ app/components/CustomText.js
@@ -1,14 +1,9 @@
-import { Text, StyleSheet, Platform } from 'react-native';
+import { Text } from 'react-native';
+
+import defaultStyles from '../config/styles';
 
 const CustomText = ({ children, style }) => {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 };
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
-  },
-});
 
 export default CustomText;
